9ab16a5bcd944e97a5250ee007eea0c0e84eb7ab
Alan Minusculi <alanminusculi@icloud.com>
2019-01-11 08:35:38 -0200

    post list template

--- myblog/core/models.py
+++ myblog/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -14,4 +15,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
