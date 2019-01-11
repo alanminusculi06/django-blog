d0a19ec1d451a5495c753faaaeb2d3448e78a54d
Alan Minusculi <alanminusculi@icloud.com>
2019-01-11 11:46:26 -0200

    post detail

--- myblog/core/urls.py
+++ myblog/core/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-     path('', views.post_list, name='post_list'),
+     path('', views.post_list, name='post_list'),     
+     path('post/<int:pk>/', views.post_detail, name='post'),
 ]
