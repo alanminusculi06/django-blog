a70442cf7aff73ab7e54a4b742135099a924bf11
Alan Minusculi <alanminusculi@icloud.com>
2019-01-11 13:42:35 -0200

    teste url

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myblog.core.urls')),
+    path('', include('myblog.core.urls', namespace='core')),
 ]
